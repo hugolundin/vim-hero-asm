1d3638b23a31fd8f9bc7bf5c256a0bb34b41bed0
Hugo Lundin <hugo@lundin.dev>
2021-03-14 17:14:29 +0100

    Add line parser tests

---
+++ tests/test_line_parse.py
@@ -0,0 +1,73 @@
+from src.asm_types.line import parse
+
+def test_simple():
+    line = 'ldi r0, 5 + 5'
+    parsed_line = parse(line)
+    assert(parsed_line.op == 'ldi')
+    assert(parsed_line.label == None)
+    assert(len(parsed_line.args) == 2)
+    assert(parsed_line.args[0] == 'r0')
+    assert(parsed_line.args[1] == '5 + 5')
+    
+    line = 'ldi r0, 5+5'
+    parsed_line = parse(line)
+    assert(parsed_line.op == 'ldi')
+    assert(parsed_line.label == None)
+    assert(len(parsed_line.args) == 2)
+    assert(parsed_line.args[0] == 'r0')
+    assert(parsed_line.args[1] == '5+5')
+
+    line = 'ld r0, r1'
+    parsed_line = parse(line)
+    assert(parsed_line.op == 'ld')
+    assert(parsed_line.label == None)
+    assert(len(parsed_line.args) == 2)
+    assert(parsed_line.args[0] == 'r0')
+    assert(parsed_line.args[1] == 'r1')
+
+def test_with_comment():
+    line = 'nop # hello world'
+    parsed_line = parse(line)
+    assert(parsed_line.op == 'nop')
+    assert(parsed_line.label == None)
+    assert(len(parsed_line.args) == 0)
+
+def test_with_label():
+    line = 'start: nop'
+    parsed_line = parse(line)
+    assert(parsed_line.op == 'nop')
+    assert(len(parsed_line.args) == 0)
+    assert(parsed_line.label == 'start')
+
+def test_with_comment_and_label():
+    line = 'start: nop # hello world'
+    parsed_line = parse(line)
+    assert(parsed_line.op == 'nop')
+    assert(len(parsed_line.args) == 0)
+    assert(parsed_line.label == 'start')
+
+def test_with_comment_and_args():
+    line = 'ldi r0, 0x5 # hello world'
+    parsed_line = parse(line)
+    assert(parsed_line.op == 'ldi')
+    assert(len(parsed_line.args) == 2)
+    assert(parsed_line.args[0] == 'r0')
+    assert(parsed_line.args[1] == '0x5')
+    
+def test_with_label_and_args():
+    line = 'start: ldi r0, 0x5'
+    parsed_line = parse(line)
+    assert(parsed_line.op == 'ldi')
+    assert(parsed_line.label == 'start')
+    assert(len(parsed_line.args) == 2)
+    assert(parsed_line.args[0] == 'r0')
+    assert(parsed_line.args[1] == '0x5')
+
+def test_with_comment_label_and_args():
+    line = 'start: ldi r0, 0x5 # hello world'
+    parsed_line = parse(line)
+    assert(parsed_line.op == 'ldi')
+    assert(parsed_line.label == 'start')
+    assert(len(parsed_line.args) == 2)
+    assert(parsed_line.args[0] == 'r0')
+    assert(parsed_line.args[1] == '0x5')
